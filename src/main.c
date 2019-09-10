1110849011e92114defcd70dc0562c250ebd252a
Morgane GUERREAU <mguerrea@student.42.fr>
2019-09-10 14:14:55 +0200

    norme main.c

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 /*   By: mguerrea <mguerrea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 14:37:54 by mguerrea          #+#    #+#             */
-/*   Updated: 2019/09/10 13:10:08 by mguerrea         ###   ########.fr       */
+/*   Updated: 2019/09/10 14:14:30 by mguerrea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "sh_parser.h"
 
-const char	*g_builtin_lst[] = {"cd", "exit", "echo", "env", "setenv",
+const char		*g_builtin_lst[] = {"cd", "exit", "echo", "env", "setenv",
 	"unsetenv"};
 const t_built_in g_builtin_fct[] = {ft_cd, ft_exit, ft_echo, ft_env,
 	ft_setenv, ft_unsetenv};
@@ -22,7 +22,6 @@ int		execute(t_cmdlst *cmd, char ***environ)
 {
 	int i;
 	int ret;
-	pid_t pid;
 
 	i = 0;
 	ret = 1;
@@ -34,15 +33,13 @@ int		execute(t_cmdlst *cmd, char ***environ)
 	}
 	if (((cmd->pipes & PIPE_R) && i < NB_BUILTIN) || i == NB_BUILTIN)
 	{
-		if ((pid = do_pipe(cmd)) == -1)
+		if ((g_pid = do_pipe(cmd)) == -1)
 			throw_error("fork error");
-		if (pid == 0)
+		if (g_pid == 0)
 		{
-			if (i < NB_BUILTIN)
-				ret = g_builtin_fct[i](cmd, environ);
-			else
-				ret = launch_bin(cmd, environ);
-			exit (0);
+			ret = (i < NB_BUILTIN) ? g_builtin_fct[i](cmd, environ) :
+				launch_bin(cmd, environ);
+			exit(0);
 		}
 	}
 	else
@@ -50,13 +47,29 @@ int		execute(t_cmdlst *cmd, char ***environ)
 	return (ret);
 }
 
-int	run(char ***env)
+int		iter_cmd(t_cmdlst *cmd, int run, char ***env)
+{
+	int *saved;
+
+	while (cmd && run)
+	{
+		if (!(saved = save_fd(cmd)))
+			malloc_error();
+		format_args(cmd, *env);
+		create_files(cmd);
+		run = execute(cmd, env);
+		restore_fd(cmd, saved);
+		cmd = cmd->next;
+	}
+	return (run);
+}
+
+int		run(char ***env)
 {
 	int			run;
 	t_token		*tknlst;
 	t_cmdlst	*cmd;
-	int			*saved;
-	t_history *history;
+	t_history	*history;
 
 	history = NULL;
 	run = 1;
@@ -68,23 +81,14 @@ int	run(char ***env)
 			break ;
 		tknlst = tokenize_line(history->line);
 		cmd = parse(tknlst);
-		while (cmd && run)
-		{
-			if (!(saved = save_fd(cmd)))
-				malloc_error();
-			format_args(cmd, *env);
-			create_files(cmd);
-			run = execute(cmd, env);
-			restore_fd(cmd, saved);
-			cmd = cmd->next;
-		}
+		run = iter_cmd(cmd, run, env);
 		// we have to free cmd and tknlst
 	}
 	free_history(&history);
 	return (0);
 }
 
-int	main(int argc, char **argv, char **environ)
+int		main(int argc, char **argv, char **environ)
 {
 	char		**env;
 
